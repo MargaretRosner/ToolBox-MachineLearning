29b1af524f75b3fb379239aa4f472dce6d9827f6
MargaretRosner <rosner.margaret@gmail.com>
2017-03-13 00:28:46 -0400

    turning in toolbox

--- learning_curve.py
+++ learning_curve.py
@@ -2,10 +2,10 @@
 
 import matplotlib.pyplot as plt
 import numpy
+import statistics
 from sklearn.datasets import *
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
-
 
 def display_digits():
     digits = load_digits()
@@ -22,7 +22,8 @@ def train_model():
     data = load_digits()
     num_trials = 10
     train_percentages = range(5, 95, 5)
-    test_accuracies = numpy.zeros(len(train_percentages))
+    #print(train_percentages)
+    test_accuracies = []
 
     # train models with training percentages between 5 and 90 (see
     # train_percentages) and evaluate the resultant accuracy for each.
@@ -31,7 +32,26 @@ def train_model():
     # For consistency with the previous example use
     # model = LogisticRegression(C=10**-10) for your learner
 
-    # TODO: your code here
+    for p in train_percentages:
+        #print(p)
+        ty = []
+        for x in range(0,num_trials):
+
+            X_train, X_test, y_train, y_test = train_test_split(data.data, data.target,
+            train_size = p)
+            model = LogisticRegression(C=10**-10)
+            model.fit(X_train, y_train)
+            #print("Train accuracy %f" %model.score(X_train, y_train))
+            #print("Test accuracy %f"%model.score(X_test, y_test))
+            y = model.score(X_test, y_test)
+
+            ty.append(y)
+
+            #print(test_accuracies)
+
+            #print(num_trials)
+        ave = statistics.mean(ty)
+        test_accuracies.append(ave)
 
     fig = plt.figure()
     plt.plot(train_percentages, test_accuracies)
@@ -39,8 +59,7 @@ def train_model():
     plt.ylabel('Accuracy on Test Set')
     plt.show()
 
-
 if __name__ == "__main__":
     # Feel free to comment/uncomment as needed
-    display_digits()
-    # train_model()
+    #display_digits()
+    train_model()
